b88984c90f780072fcbcaba811825cecd2b448ce
Ankon190 <ankon190@gmail.com>
2025-11-13 21:02:47 +0600

    Ädded choose login as file with css

---
+++ SelectLoginType.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Login Type Select
+        </title>
+        <style href="SelectLoginTypeStyle: .css"></style>
+    </head>
+    <body>
+       <div class="mainSection">
+        <h1 class="heading">Select for login as: </h1>
+            <button class="patient">Patient</button>
+            <button class="doctor">Doctor</button>
+       </div> 
+
+
+
+    </body>
+</html>
