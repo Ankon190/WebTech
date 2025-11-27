291e7aebe3a2ebd21ec698c704f869b1b168f31c
Ankon 190 <ankon190@gmail.com>
2025-11-27 14:10:40 +0600

    Navbar almost finished. Script part is left

--- Home.php
+++ Home.php
@@ -3,6 +3,7 @@
 <head>
     <title>Home</title>
     <link rel="stylesheet" href="HomeStyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         
     </style>
@@ -16,6 +17,23 @@
         <li><a href="#">About Us</a></li>
         <li><a href="Login.php">Login</a></li>
     </ul>
+
+    <div class="hamburger-menu" onclick="toggleDropdown()">
+        <i class="fas fa-bars"></i>
+    </div>
+
+    <div class="dropdown-Content"  id="dropdownMenu">
+        
+            <a class="active" href="Home.php">Home</a>
+            <a href="#">Profile</a>
+            <a href="#">Reports</a>
+            <a href="#">About Us</a>
+            <a href="Login.php">Login</a>
+            <a href="#">Logout</a>
+        
+    </div>
     </nav>
+
+
 </body>
 </html>
