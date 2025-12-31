b6246fa3bee90830ebdd578f2fb9b072e61dcdc6
Ankon 190 <ankon190@gmail.com>
2025-12-31 10:47:42 +0600

    display username in the homepage from database

--- HomePage.php
+++ HomePage.php
@@ -3,18 +3,27 @@
 require_once 'db_connect.php';
 //session created
 session_start();
-
 if(!isset($_SESSION['username'])){
     header("Location: Login.php");
     exit();
 }
+else{
+    $username = $_SESSION['username'];
+    $stmt = $conn->prepare("SELECT user_name FROM users WHERE user_name = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $user_result = $stmt -> get_result();
 
-
+    if($user_result ->num_rows ===1){
+        $user = $user_result -> fetch_assoc();
+    }
+    $stmt->close();
 //declaring variables
-    $patient_name = "John Doe";
+    $patient_name = $user['user_name'];
     $appointed_doctor = "Dr. Smith";
     $specialization = "Cardiologist";
     $appointment_date = "2025-12-31";
+}
 ?>
 
 
