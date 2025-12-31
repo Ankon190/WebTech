e71863f76f7d8263d223af67ae1d56c91275da58
Ankon 190 <ankon190@gmail.com>
2025-12-31 20:23:27 +0600

    added feature that blocks if username is same when registration

--- reg_validation.php
+++ reg_validation.php
@@ -37,34 +37,45 @@
         else{
             $has_error = false;
         }
+
         //check if there is no error
         if($has_error==false){
             //clear error message for stop displaying error msg
             $errormsg = "";
-            //encript password
-            $hashPassword = password_hash($password, PASSWORD_DEFAULT);
-
-            //check if the user is patient or doctor
-            if($user === "patient"){
-               //data insertion query in tables
-                $reg_insert_query = "INSERT INTO patient (user_name,user_email, dob, gender, blood_group, weight, address) VALUES ('$name', '$email', '$dob', '$gender', '$bloodgroup', '$weight', '$address')";
-                $reg_insert_user_query = "INSERT INTO users (user_name, email, password, user_type) VALUES ('$name', '$email', '$hashPassword', '$user')";
-
-                if($conn->query($reg_insert_query) && $conn->query($reg_insert_user_query)) {               
-                    $success="Registration Complete you can do login";
-                } else {
-                    $errormsg = "Error: " . mysqli_error($conn);
+            //check if the user already exists in the database
+                $check_user_exist = "SELECT * FROM users WHERE user_name='$name'";
+                $result = $conn->query($check_user_exist);
+                if($result->num_rows > 0){
+                    $errormsg = "Username or Email already exists";
+                    $has_error = true;
                 }
-            }
-            elseif($user === "doctor"){
+            
+            else{
+                //encript password
+                $hashPassword = password_hash($password, PASSWORD_DEFAULT);
+                //check if the user is patient or doctor
+                if($user === "patient"){
                 //data insertion query in tables
-                $reg_insert_query = "INSERT INTO doctor (user_name, dob, gender, blood_group, weight, address) VALUES ('$name', '$dob', '$gender', '$bloodgroup', '$weight', '$address')";
-                $reg_insert_user_query = "INSERT INTO users (user_name, email, password, user_type) VALUES ('$name', '$email', '$hashPassword', '$user')";
+                    $reg_insert_query = "INSERT INTO patient (user_name,user_email, dob, gender, blood_group, weight, address) VALUES ('$name', '$email', '$dob', '$gender', '$bloodgroup', '$weight', '$address')";
+                    $reg_insert_user_query = "INSERT INTO users (user_name, email, password, user_type) VALUES ('$name', '$email', '$hashPassword', '$user')";
 
-                if($conn->query($reg_insert_query) && $conn->query($reg_insert_user_query)) {               
-                    $success="Registration Complete you can do login";
-                } else {
-                    $errormsg = "Error: " . mysqli_error($conn);
+                    if($conn->query($reg_insert_query) && $conn->query($reg_insert_user_query)) {               
+                        $success="Registration Complete you can do login";
+                    } else {
+                        $errormsg = "Error: " . mysqli_error($conn);
+                    }
+                }
+                elseif($user === "doctor"){
+                    //data insertion query in tables
+                    $reg_insert_query = "INSERT INTO doctor (user_name, dob, gender, blood_group, weight, address) VALUES ('$name', '$dob', '$gender', '$bloodgroup', '$weight', '$address')";
+                    $reg_insert_user_query = "INSERT INTO users (user_name, email, password, user_type) VALUES ('$name', '$email', '$hashPassword', '$user')";
+
+                    if($conn->query($reg_insert_query) && $conn->query($reg_insert_user_query)) {               
+                        $success="Registration Complete you can do login";
+                    } 
+                    else {
+                        $errormsg = "Error: " . mysqli_error($conn);
+                    }
                 }
             }
     }
