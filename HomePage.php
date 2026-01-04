095b2ccab3c73f0e55a0d699a8010030fe80e41e
Ankon 190 <ankon190@gmail.com>
2026-01-04 21:07:40 +0600

    homepage database code separated to externalfile and linked with homepage

--- HomePage.php
+++ HomePage.php
@@ -1,30 +1,7 @@
 <?php
 //database connection
-require_once 'db_connect.php';
-//session created
-session_start();
-if(!isset($_SESSION['username'])){
-    header("Location: Login.php");
-    exit();
-}
-
-else{
-    $username = $_SESSION['username'];
-    $stmt = $conn->prepare("SELECT user_name FROM users WHERE user_name = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $user_result = $stmt -> get_result();
-
-    if($user_result ->num_rows ===1){
-        $user = $user_result -> fetch_assoc();
-    }
-    $stmt->close();
-//declaring variables
-    $patient_name = $user['user_name'];
-    $appointed_doctor = "Dr. Smith";
-    $specialization = "Cardiologist";
-    $appointment_date = "2025-12-31";
-}
+require 'db_connect.php';
+require 'homepageGetData.php';
 ?>
 
 
