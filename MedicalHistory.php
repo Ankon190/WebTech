b9e46b080ddbce0e5dd1bff4c80f356cca639fa3
Ankon 190 <ankon190@gmail.com>
2026-01-04 21:33:30 +0600

    medical history database code separated in external file and linked with medicalhistory page

--- MedicalHistory.php
+++ MedicalHistory.php
@@ -1,16 +1,7 @@
 <?php
 //database connection
 require 'db_connect.php';
-
-//session created
-session_start();
-
-if(!isset($_SESSION['username'])){
-    header("Location: Login.php");
-    exit();
-}
-//declaring variables
-    $patient_name = "John Doe";
+require 'medicalHistoryGetData.php';
 
 ?>
 <!DOCTYPE html>
